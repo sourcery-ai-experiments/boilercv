fe2b857fe80495fd5fe4da56ff7e0b8534b9f6be
blakeNaccarato <blake.naccarato@gmail.com>
2022-06-15 11:47:54 -0700

    Update setup script for WSL

--- setup_wsl.ps1
+++ setup_wsl.ps1
@@ -1,8 +1,8 @@
 rm -rf .venv
 python3 -m venv .venv --upgrade-deps
 .venv/bin/Activate.ps1
-pip install -U setuptools wheel  # must be done separately from above
 pip install -U -r .tools/requirements/requirements_dev.txt
-pip uninstall -y boilercv
 pip install -e .
 pip install --force-reinstall /home/user/opencv-python/dist/opencv_contrib_python-4.6.0.66-cp310-cp310-linux_x86_64.whl
+pre-commit install
+pre-commit
