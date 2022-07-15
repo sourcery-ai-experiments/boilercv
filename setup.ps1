29bce9df3e8050b5e4555bc559d6ca7e1e119116
blakeNaccarato <blake.naccarato@gmail.com>
2022-07-15 14:29:14 -0700

    Update setup scripts

--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,6 @@
-py -3.10 -m venv .venv --clear --upgrade-deps
+$GLOBAL_PYTHON = 'py -3.10'
+try { Invoke-Expression "$GLOBAL_PYTHON --version" } catch [System.Management.Automation.CommandNotFoundException] { $GLOBAL_PYTHON = 'python3.10' }
+Invoke-Expression "$GLOBAL_PYTHON -m venv .venv --clear --upgrade-deps"
 . ./update.ps1
+pre-commit install
+pre-commit
--- update.ps1
+++ update.ps1
@@ -1,6 +1,18 @@
-.venv/Scripts/activate
+$VENV_ACTIVATE_WINDOWS = '.venv/Scripts/activate'
+$VENV_ACTIVATE_LINUX = '.venv/bin/Activate.ps1'
+
+if ( Test-Path $VENV_ACTIVATE_WINDOWS ) { . $VENV_ACTIVATE_WINDOWS }
+elseif ( Test-Path $VENV_ACTIVATE_LINUX ) { . $VENV_ACTIVATE_LINUX }
+else {
+    throw [System.Management.Automation.ItemNotFoundException] 'Could not find a virtual environment.'
+}
 python -m pip install -U pip  # instructed to do this by pip
 pip install -U setuptools wheel  # must be done separately from above
 pip install -U -r .tools/requirements/requirements_dev.txt
 pip uninstall -y boilercv
+python .tools/scripts/bump_pyproject.py
 pip install -e .
+
+# If we are in WSL, replace required OpenCV with the debug build
+$OPENCV_DEBUG_BUILD = '/home/user/opencv-python/dist/opencv_contrib_python-4.6.0.66-cp310-cp310-linux_x86_64.whl'
+if ( Test-Path $OPENCV_DEBUG_BUILD ) { pip install --force-reinstall $OPENCV_DEBUG_BUILD }
