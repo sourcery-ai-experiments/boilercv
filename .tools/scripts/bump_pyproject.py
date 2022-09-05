62ae1f2f6684851f31e8836e0563cfb26b024569
blakeNaccarato <blake.naccarato@gmail.com>
2022-09-04 19:58:17 -0700

    Bump template

--- .tools/scripts/bump_pyproject.py
+++ .tools/scripts/bump_pyproject.py
@@ -8,7 +8,10 @@ REQUIREMENTS = Path("requirements.txt")
 PYPROJECT = Path("pyproject.toml")
 
 with open(REQUIREMENTS) as file:
-    dependencies = [line.rstrip() for line in file if not line.startswith("#")]
+    dependencies = [
+        line.rstrip() for line in file if line != "\n" and not line.startswith("#")
+    ]
+
 
 with open(PYPROJECT) as file:
     content = toml.load(file)
