52765d22258ace55bb054ef84f17873abe8c7581
blakeNaccarato <blake.naccarato@gmail.com>
2023-03-07 15:34:36 -0800

    Enable DVC hooks locally

--- update.ps1
+++ update.ps1
@@ -2,13 +2,16 @@
 Update the local virtual environment to the latest tracked dependencies.
 #>
 
+# * ------------------------------------------------------------------------------ * #
+# * Changes below may be lost in significant template updates.
+
 # Activate environment
 $VENV_ACTIVATE_WINDOWS = '.venv/Scripts/activate'
 $VENV_ACTIVATE_UNIX = '.venv/bin/Activate.ps1'
 if ( Test-Path $VENV_ACTIVATE_WINDOWS ) { . $VENV_ACTIVATE_WINDOWS }
 elseif ( Test-Path $VENV_ACTIVATE_UNIX ) { . $VENV_ACTIVATE_UNIX }
 else {
-throw [System.Management.Automation.ItemNotFoundException] 'Could not find a virtual environment.'
+    throw [System.Management.Automation.ItemNotFoundException] 'Could not find a virtual environment.'
 }
 
 # Install dev requirements
@@ -24,3 +27,8 @@ pip install --upgrade --requirement 'requirements.txt'
 
 # Ensure pre-commit hooks are applied and updated
 pre-commit install --install-hooks
+
+# * -------------------------------------------------------------------------------- * #
+# * Changes below should persist in significant template updates.
+
+pre-commit install --install-hooks --hook-type pre-push --hook-type post-checkout
