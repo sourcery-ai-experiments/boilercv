a6f6c52b6db98e81f33e2267298e8d4bbe4762ef
blakeNaccarato <blake.naccarato@gmail.com>
2023-03-08 12:57:15 -0800

    Bump template

--- .tools/scripts/Update-Template.ps1
+++ .tools/scripts/Update-Template.ps1
@@ -4,4 +4,4 @@ Update the project to the latest template version.
 
 & '.tools/scripts/template_common.ps1'
 copier -f -r "$(git rev-parse HEAD:template)"
-python compose_pyproject.py
+python '.tools/scripts/compose_pyproject.py'
