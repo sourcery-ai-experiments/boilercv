19a6a2a3c264d44575a232dbac4325c4a31e5254
Blake Naccarato <blake.naccarato@gmail.com>
2023-03-07 21:32:04 -0800

    Restore update.ps1

--- update.ps1
+++ update.ps1
@@ -30,3 +30,5 @@ pre-commit install --install-hooks
 
 # * -------------------------------------------------------------------------------- * #
 # * Changes below should persist in significant template updates.
+
+pre-commit install --install-hooks --hook-type pre-push --hook-type post-checkout
