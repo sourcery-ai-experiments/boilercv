3e978a64845bb75078f29e5f4ac98f5e8616c389
blakeNaccarato <blake.naccarato@gmail.com>
2023-03-13 16:19:28 -0700

    Get latest pycine

---
+++ src/boilercv/examples/cine.py
@@ -0,0 +1,21 @@
+import cv2
+from pycine.raw import read_frames
+
+
+def imshow(img):
+    _, ret = cv2.imencode(".jpg", img * 255)
+    cv2.imshow("image", ret)
+
+
+def main():
+    raw_images, *_ = read_frames(
+        cine_file="C:/Users/Blake/Desktop/test/results_2022-01-06T13-23-39.cine",
+        start_frame=0,
+        count=10,
+    )
+    for _image in raw_images:
+        ...
+
+
+if __name__ == "__main__":
+    main()
