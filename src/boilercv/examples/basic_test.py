784cdc65f08b53dbd449fd2a14193b47c99dfb02
blakeNaccarato <blake.naccarato@gmail.com>
2023-03-16 12:17:38 -0700

    Fix basic test example

--- src/boilercv/examples/basic_test.py
+++ src/boilercv/examples/basic_test.py
@@ -6,14 +6,15 @@ from pathlib import Path
 import cv2 as cv
 import numpy as np
 
-from boilercv.examples import bgr_to_rgb, video_capture_images
+from boilercv.examples import bgr_to_rgb, qt_window, video_capture_images
 from boilercv.examples.contours.pyqtgraph_cine import get_roi
 
 
 def main():
     with video_capture_images(Path(cv.samples.findFile("vtest.avi"))) as images:
         video = np.stack([bgr_to_rgb(image) for image in images])
-    get_roi(video)
+    with qt_window() as (app, window):
+        get_roi(video, app, window)
 
 
 if __name__ == "__main__":
