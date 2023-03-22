d1e3db58084f5bcbfef62166d417d7a16759b988
blakeNaccarato <blake.naccarato@gmail.com>
2023-03-22 13:27:44 -0700

    Switch example image source to NetCDF

--- src/boilercv/examples/contours/__init__.py
+++ src/boilercv/examples/contours/__init__.py
@@ -1,9 +1,13 @@
 """Contour-finding examples."""
 
+import xarray as xr
+
 from boilercv import get_8bit_images
-from boilercv.gui import get_video_images
 from boilercv.models.params import PARAMS
 
-IMAGES = get_8bit_images(
-    get_video_images(PARAMS.paths.examples / "results_2022-11-30T12-39-07_98C.cine")
-)
+with xr.open_dataset(
+    PARAMS.paths.examples / "results_2022-11-30T12-39-07_98C.nc"
+) as ds:
+    IMAGES = get_8bit_images(ds.images.data)
+
+ROI_FILE = PARAMS.paths.examples / "roi.yaml"
