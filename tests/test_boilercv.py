25d6bc8c1ea9c85910cd77c4c8b035d4ed279939
blakeNaccarato <blake.naccarato@gmail.com>
2023-04-07 02:14:21 -0700

    Don't pull from remote in CI. Costs too much!

--- tests/test_boilercv.py
+++ tests/test_boilercv.py
@@ -1,11 +1,3 @@
-from subprocess import run
-
-
 def test_import():
     """Import test."""
     import boilercv  # noqa: F401
-
-
-def test_repro_check_cv():
-    """Run the basic OpenCV examples."""
-    run(["pwsh", "-C", ".venv/Scripts/activate; dvc repro --pull --force check_cv"])
