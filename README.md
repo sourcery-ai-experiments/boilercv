cf5122f4bed6e5b34ef16de460f30615f0813f67
blakeNaccarato <blake.naccarato@gmail.com>
2023-04-13 12:08:56 -0700

    Update DOI

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # boilercv
 
+[![DOI](https://zenodo.org/badge/503551174.svg)](https://zenodo.org/badge/latestdoi/503551174)
+
 Computer vision routines suitable for nucleate pool boiling bubble analysis.
--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,12 @@
 from datetime import date
 
-from myst_parser import __version__
-
 project = ""
 html_title = "Contents"
 copyright = f"{date.today().year}, Blake Naccarato, Kwang Jin Kim"  # noqa: A001
-version = __version__
+version = "0.0.0"
 master_doc = "index"
 language = "en"
 extensions = ["myst_parser", "sphinx_design"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_book_theme"
+extensions = ["myst_parser", "sphinx_design", "sphinxcontrib.mermaid"]
