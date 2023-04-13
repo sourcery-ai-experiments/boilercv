24a245fe264fbab299dd98424ecafa602f5477bb
blakeNaccarato <blake.naccarato@gmail.com>
2023-04-13 12:30:58 -0700

    Fix duplicate extensions specification

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,6 @@ copyright = f"{date.today().year}, Blake Naccarato, Kwang Jin Kim"  # noqa: A001
 version = "0.0.0"
 master_doc = "index"
 language = "en"
-extensions = ["myst_parser", "sphinx_design"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_book_theme"
 extensions = ["myst_parser", "sphinx_design", "sphinxcontrib.mermaid"]
