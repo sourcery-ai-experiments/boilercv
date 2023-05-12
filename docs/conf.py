f09e2001024de8d94af68fbc6de7d10009fb46e2
blakeNaccarato <blake.naccarato@gmail.com>
2023-05-12 09:58:35 -0700

    Reconcile template evolution differences

--- docs/conf.py
+++ docs/conf.py
@@ -9,3 +9,12 @@ language = "en"
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_book_theme"
 extensions = ["myst_parser", "sphinx_design", "sphinxcontrib.mermaid"]
+# https://sphinx-book-theme.readthedocs.io/en/stable/reference.html#reference-of-theme-options
+html_theme_options = {
+    "path_to_docs": "docs",
+    "repository_url": "https://github.com/blakeNaccarato/boilercv",
+    "repository_branch": "main",
+    "use_download_button": True,
+    "use_fullscreen_button": True,
+    "use_repository_button": True,
+}
