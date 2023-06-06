b213329f61155fa78b019856685e6518a8d5a05a
blakeNaccarato <blake.naccarato@gmail.com>
2023-06-05 21:24:18 -0700

    Add tests

--- tests/test_boilercv.py
+++ tests/test_boilercv.py
@@ -10,6 +10,17 @@ import pytest
 @pytest.mark.usefixtures("tmp_project")
 @pytest.mark.parametrize(
     "stage",
+    [stage.stem for stage in Path("src/boilercv/manual").glob("[!__]*.py")],
+)
+def test_manual(stage: str):
+    """Test that manual stages can run."""
+    importlib.import_module(f"boilercv.manual.{stage}").main()
+
+
+@pytest.mark.slow()
+@pytest.mark.usefixtures("tmp_project")
+@pytest.mark.parametrize(
+    "stage",
     [
         stage.stem
         for stage in Path("src/boilercv/stages").glob("[!__]*.py")
@@ -28,9 +39,19 @@ def test_stages(stage: str):
     [
         stage.stem
         for stage in Path("src/boilercv/stages/update_previews").glob("[!__]*.py")
-        if stage.stem not in {}
     ],
 )
 def test_update_previews(stage: str):
     """Test that preview update stages can run."""
     importlib.import_module(f"boilercv.stages.update_previews.{stage}").main()
+
+
+@pytest.mark.slow()
+@pytest.mark.usefixtures("tmp_project")
+@pytest.mark.parametrize(
+    "stage",
+    [stage.stem for stage in Path("src/boilercv/previews").glob("[!__]*.py")],
+)
+def test_previews(stage: str):
+    """Test that manual stages can run."""
+    importlib.import_module(f"boilercv.previews.{stage}").main()
