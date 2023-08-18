7971658065c3b714ae40c35d2b2c843727cb451f
blakeNaccarato <blake.naccarato@gmail.com>
2023-08-18 16:11:48 -0700

    Introduce local variation in `pyrightconfig.json`

---
+++ .tools/scripts/local_pyrightconfig.py
@@ -0,0 +1,18 @@
+"""Generate a local `pyrightconfig.json` variation for development."""
+
+from json import dumps
+from pathlib import Path
+from tomllib import loads
+
+
+def main():
+    pyright = loads(Path("pyproject.toml").read_text("utf-8"))["tool"]["pyright"]
+    pyright["include"] += [f"../boilercore/{path}" for path in pyright["include"]]
+    Path("pyrightconfig.json").write_text(
+        encoding="utf-8",
+        data=f"{dumps(indent=2, obj=pyright)}\n",
+    )
+
+
+if __name__ == "__main__":
+    main()
