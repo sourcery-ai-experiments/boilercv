dd92223d085a57d5855738ddf7d282658ad13550
blakeNaccarato <blake.naccarato@gmail.com>
2023-10-28 18:03:19 -0700

    Simplify call to get_times

--- src/boilercv/stages/experiments/e230920_subcool/__main__.py
+++ src/boilercv/stages/experiments/e230920_subcool/__main__.py
@@ -10,19 +10,18 @@ from boilercv.stages.experiments.e230920_subcool import EXP, get_times
 
 NB = fold(PARAMS.paths.stages[f"experiments_{EXP}_find_collapse"])
 PATHS = (PARAMS.paths.experiments / EXP).iterdir()
-TIMES = dict(zip(get_times(path.stem for path in PATHS), PATHS, strict=True))
 
 
 def main():
     if not modified(NB):
         return
     with ProcessPoolExecutor() as executor:
-        for time in TIMES.values():
+        for dt in get_times(path.stem for path in PATHS):
             executor.submit(
                 execute_notebook,
                 input_path=NB,
                 output_path=None,
-                parameters={"TIME": time},
+                parameters={"TIME": dt.isoformat()},
             )
 
 
