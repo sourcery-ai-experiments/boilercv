ee48ef217addf9ff3d557240b6142d769290dc96
blakeNaccarato <blake.naccarato@gmail.com>
2023-10-28 17:05:40 -0700

    Get times from filenames

--- src/boilercv/stages/experiments/e230920_subcool/__init__.py
+++ src/boilercv/stages/experiments/e230920_subcool/__init__.py
@@ -1,6 +1,15 @@
 """Subcooled bubble collapse experiment."""
 
-from boilercore.paths import get_module_name
+from collections.abc import Iterable
+from datetime import datetime
+
+from boilercore.paths import ISOLIKE, dt_fromisolike, get_module_name
 
 EXP = get_module_name(__spec__ or __file__)
 """Name of this experiment."""
+
+
+def get_times(strings: Iterable[str]) -> Iterable[datetime]:
+    for string in strings:
+        if match := ISOLIKE.search(string):
+            yield dt_fromisolike(match)
--- src/boilercv/stages/experiments/e230920_subcool/__main__.py
+++ src/boilercv/stages/experiments/e230920_subcool/__main__.py
@@ -6,27 +6,18 @@ from boilercore.paths import fold, modified
 from ploomber_engine import execute_notebook
 
 from boilercv.models.params import PARAMS
-from boilercv.stages.experiments.e230920_subcool import EXP
+from boilercv.stages.experiments.e230920_subcool import EXP, get_times
 
 NB = fold(PARAMS.paths.stages[f"experiments_{EXP}_find_collapse"])
-TIMES = [
-    "2023-09-20T16:52:06",
-    "2023-09-20T16:52:06",
-    "2023-09-20T17:01:04",
-    "2023-09-20T17:05:15",
-    "2023-09-20T17:14:18",
-    "2023-09-20T17:26:15",
-    "2023-09-20T17:34:38",
-    "2023-09-20T17:42:19",
-    "2023-09-20T17:47:49",
-]
+PATHS = (PARAMS.paths.experiments / EXP).iterdir()
+TIMES = dict(zip(get_times(path.stem for path in PATHS), PATHS, strict=True))
 
 
 def main():
     if not modified(NB):
         return
     with ProcessPoolExecutor() as executor:
-        for time in TIMES:
+        for time in TIMES.values():
             executor.submit(
                 execute_notebook,
                 input_path=NB,
