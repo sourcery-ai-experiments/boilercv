735e53d1fc0b34460ef93a1addefafeac8c9487d
blakeNaccarato <blake.naccarato@gmail.com>
2024-01-25 15:27:20 -0800

    Update local Pyright config generation

--- .tools/scripts/local_pyrightconfig.py
+++ .tools/scripts/local_pyrightconfig.py
@@ -1,17 +1,46 @@
-"""Generate a local `pyrightconfig.json` variation for development."""
+"""Make a local `pyrightconfig.json` that includes the specified paths."""
 
+from collections.abc import Iterable
 from json import dumps
 from pathlib import Path
 from tomllib import loads
 
+PYPROJECT = Path("pyproject.toml")
+"""Default `pyproject.toml`."""
+PYRIGHTCONFIG = Path("pyrightconfig.json")
+"""Default `pyrightconfig.json`."""
+OTHERS = [Path("../boilercore/src")]
+"""Default local paths to append."""
 
-def main():
-    pyright = loads(Path("pyproject.toml").read_text("utf-8"))["tool"]["pyright"]
-    pyright["include"] += [f"../boilercore/{path}" for path in pyright["include"]]
-    Path("pyrightconfig.json").write_text(
-        encoding="utf-8", data=f"{dumps(indent=2, obj=pyright)}\n"
-    )
+
+def make_local_pyrightconfig(
+    pyproject: Path | str = PYPROJECT,
+    pyrightconfig: Path = PYRIGHTCONFIG,
+    others: Iterable[Path | str] = OTHERS,
+):
+    """Make a local `pyrightconfig.json` that includes the specified paths.
+
+    Duplicates config from `pyproject.toml` to `pyrightconfig.json` and appends
+    specified paths to `include`. Enables type checking and refactoring across project
+    boundaries, for instance with editable local dependencies during development of
+    related projects in a multi-repo workflow.
+
+    Args:
+        pyproject: Source `pyproject.toml`. Search project root by default.
+        pyrightconfig: Destination `pyrightconfig.json`. Search project root by default.
+        others: Local paths to append. Defaults to `[Path("../boilercore/src")]`.
+    """
+    config = loads(Path(pyproject).read_text("utf-8"))["tool"]["pyright"]
+    config = {
+        "include": [
+            *config.pop("include", []),
+            *[str(Path(other).as_posix()) for other in others],
+        ],
+        **config,
+    }
+    data = f"{dumps(indent=2, obj=config)}\n"
+    Path(pyrightconfig).write_text(encoding="utf-8", data=data)
 
 
 if __name__ == "__main__":
-    main()
+    make_local_pyrightconfig()
